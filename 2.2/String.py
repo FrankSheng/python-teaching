1115f126103fda45041d594d2a40bd43730253c9
FrankSheng <17621763266@163.com>
2019-07-07 17:40:54 +0800

    Create String.py
    2.2课时代码

---
+++ 2.2/String.py
@@ -0,0 +1,16 @@
+message="I am Frank"
+
+# print(message.title())
+# print(message.upper())
+# print(message.lower())
+
+mss="    this is message!      "
+# print(mss+message)
+# print(mss.lstrip()+message)#删除开头的空白字符
+# print(mss.rstrip()+message)
+# print(mss.strip()+message)#删除两端的空白字符
+# 错误示例
+# mss_2="i'm frank!"
+# mss_2='i'm frank!'
+mss_2='this is "a"!'
+print(mss_2)
