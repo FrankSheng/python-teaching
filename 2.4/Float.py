72d1dcc41df30d924af831cd268f1bc645d54e40
FrankSheng <17621763266@163.com>
2019-07-13 18:50:18 +0800

    Create Float.py
    浮点型讲解

---
+++ 2.4/Float.py
@@ -0,0 +1,12 @@
+# print(1.0+3.0)
+# print(1.0-3.0)
+# print(1*3)
+# print(1.0*3.0)
+# print(1.0/3.0)
+# print(2.0%3.0)
+# print(1.0+(3.0+1)*2)
+# 一个浮点数与一个整数相加或相乘他的结果还是一个浮点数
+age=23
+# message="I am "+age+" years old"
+message="I am "+str(age)+" years old"
+print(message)
