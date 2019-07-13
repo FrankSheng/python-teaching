e4598bde5c628d4290574c7e09d1a6effc918912
FrankSheng <17621763266@163.com>
2019-07-13 18:50:28 +0800

    Create complex.py
    复数讲解

---
+++ 2.5/complex.py
@@ -0,0 +1,17 @@
+a=1.0+2.0j
+b=2.0-4.0j
+# print(a+b)
+# print(a-b)
+# a*b运算过程
+# (1.0+2.0j)*(2.0-4j)
+# 1.0*2.0-1.0*4j+2.0j*2.0-2.0j*4j
+# 2-4j+4j+8
+# 10+0j
+# print(a*b)
+# print(a/b)#复数除法的运算规则可以参见https://baike.baidu.com/item/复数运算法则/2568041
+# print(a+b+(a-b)*a)
+c=complex(1,4)
+# print(c)
+# print(c.real)
+# print(c.imag)
+print(c.conjugate())
