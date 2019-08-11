dcafcb7da6e090bcda4b2d33903d08d462864207
FrankSheng <17621763266@163.com>
2019-08-11 13:16:42 +0800

    改变结构
    增加什么是列表的代码

---
+++ 3/3.1/List.py
@@ -0,0 +1,15 @@
+names=["张三","李四","小明","小强"]
+# print(names)
+# print(names[0])
+# print(names[1])
+# print(names[2])
+# print(names[3])
+# print(names[-2])
+# print(names[3]==names[-1])
+# message="我是"
+# print(message+names[0])
+# print(message+names[1])
+# print(message+names[2])
+# print(message+names[3])
+others=[1,2,"abs"]
+print(others)
