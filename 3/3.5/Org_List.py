ab80d8cfe74146e4777435065fad6c27d6f92cfe
FrankSheng <17621763266@163.com>
2019-11-17 17:27:39 +0800

    课程3.5节源码
    课程3.5节源码

---
+++ 3/3.5/Org_List.py
@@ -0,0 +1,19 @@
+# cars=["bmw","audi","toyota","subaru"]
+nums=[10,20,14,1,2,4,12]
+del nums[8]
+# print("原列表：")
+# print(nums)
+# nums.sort() #升序排序
+# nums.sort(reverse=True)
+# nums.reverse()
+# print("1反转列表：")
+# print(nums)
+# nums.reverse()#两次反转，原样输出
+# print("2反转列表：")
+# print(sorted(nums))
+# print("sorted排序后的：")
+# print(nums)
+#sort 与 reverse 区别是
+# 1.sort会对列表元素值进行排序，如果传入reverse=True，那么将对排序后的列表再进行倒序
+# 2.reverse方法只将列表元素反转，不会对内容进行逐个对比排序。
+# print("列表长度："+str(len(nums)))
