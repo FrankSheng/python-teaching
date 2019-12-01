02369dc6db31efc248eafe94ee0006ab4851ff30
FrankSheng <17621763266@163.com>
2019-12-01 21:56:50 +0800

    第四章1、2节

---
+++ 4/4.1/magician.py
@@ -0,0 +1,6 @@
+magicians=["david","alice","carolina"]
+for magician in magicians:
+    print(magician.title()+", that was a great trick!")
+print("****************")
+for index in range(0,3):
+    print(magicians[index].title()+", that was a great trick!")
---
+++ 4/4.2/list_error.py
@@ -0,0 +1,13 @@
+# names=["小明","小强","小李"]
+# for item in names:
+#     print(item+" 你好")
+# print(item)
+
+magicians=["alice","david","carolina"]
+for magician in magicians:
+    print(magician.title()+",that was a great trick!")
+    print("I can't wait to see your next trick,"+magician.title()+".\n")
+                                                                                                           
+# print("Thank you everyone,that was a great magic show!")       ##①
+
+
